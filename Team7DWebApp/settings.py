7eb075d907c8a0a055875d8698d020d37a259630
Mahnoor Qureshi <2454285q@student.gla.ac.uk>
2021-03-31 03:09:29 +0100

    Can successfully register an account

--- Team7DWebApp/settings.py
+++ Team7DWebApp/settings.py
@@ -93,6 +93,8 @@ DATABASES = {
     }
 }
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 AUTHENTICATION_BACKENDS = [
     # Needed to login by username in Django admin, regardless of `allauth`
     'django.contrib.auth.backends.ModelBackend',
